946b259a5c328d7008043302557b85df7956173b
Jhon Wayler Camizan <jhonw.lima08@gmail.com>
2024-03-14 09:50:05 -0500

    edit routes api

--- Proyecto_API/api/urls.py
+++ Proyecto_API/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CompanyView
 
 urlpatterns=[
-    path('companies/', CompanyView.as_view(), name='companies_list'),
-    path('companies/<int:id>', CompanyView.as_view(), name='companies_process')
+    path('v1/companies/', CompanyView.as_view(), name='companies_list'),
+    path('v1/companies/<int:id>', CompanyView.as_view(), name='companies_process')
 ]
